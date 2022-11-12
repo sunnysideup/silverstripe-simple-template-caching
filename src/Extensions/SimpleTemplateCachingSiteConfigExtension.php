38d44b7fa4518acc4e5ab4a544d1cc030ed63b7b
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-11-13 08:31:39 +1300

    WIP

--- src/Extensions/SimpleTemplateCachingSiteConfigExtension.php
+++ src/Extensions/SimpleTemplateCachingSiteConfigExtension.php
@@ -6,31 +6,57 @@ use SilverStripe\Forms\DatetimeField;
 use SilverStripe\Forms\ReadonlyField;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\FieldList;
+
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordViewer;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\ORM\DB;
 use SilverStripe\ORM\FieldType\DBDatetime;
 use SilverStripe\SiteConfig\SiteConfig;
 
+use SilverStripe\Core\Injector\Injector;
+
+use Sunnysideup\SimpleTemplateCaching\Model\ObjectsUpdated;
+
 class SimpleTemplateCachingSiteConfigExtension extends DataExtension
 {
     private static $db = [
         'HasCaching' => 'Boolean(1)',
+        'RecordCacheUpdates' => 'Boolean(0)',
         'CacheKeyLastEdited' => 'DBDatetime',
         'ClassNameLastEdited' => 'Varchar(200)',
     ];
 
     public function updateCMSFields(FieldList $fields)
     {
+        $name = '';
+        if(class_exists($this->getOwner()->ClassNameLastEdited)) {
+            $name = Injector::inst()->get($this->getOwner()->ClassNameLastEdited)->i18n_singular_name();
+        }
         $fields->addFieldsToTab(
             'Root.Caching',
             [
                 CheckboxField::create('HasCaching', 'Use caching'),
+                CheckboxField::create('RecordCacheUpdates', 'To work out when the cache is being updated, you can track every change. This will slow down all your edits, so it is recommend only to turn this on temporarily - for tuning purposes.'),
                 DatetimeField::create('CacheKeyLastEdited', 'Content Last Edited')
-                    ->setRightTitle('The frontend template cache will be invalidated every time this value changes.'),
+                    ->setRightTitle('The frontend template cache will be invalidated every time this value changes. It changes every time anything is changed in the database.'),
                 ReadonlyField::create('ClassNameLastEdited', 'Last class updated')
-                    ->setRightTitle('If a class is being updated too often then you can exclude it.'),
+                    ->setRightTitle('Last object updated. The name of this object is: '.$name),
             ]
         );
+        if($this->getOwner()->RecordCacheUpdates) {
+            $fields->addFieldsToTab(
+                'Root.Caching',
+                [
+                    GridField::create(
+                        'ObjectsUpdated',
+                        'Last 1000 objects updated',
+                        ObjectsUpdated::get()->limit(1000),
+                        GridFieldConfig_RecordViewer::create()
+                    )
+                ]
+            );
+        }
     }
 
     public static function site_cache_key(): string
@@ -45,11 +71,20 @@ class SimpleTemplateCachingSiteConfigExtension extends DataExtension
 
     public static function update_cache_key(?string $className = '')
     {
-        DB::query('
-            UPDATE "SiteConfig"
-            SET
-                "CacheKeyLastEdited" = \'' . DBDatetime::now()->Rfc2822() . '\',
-                "ClassNameLastEdited" = \'' . $className. '\'
-        ;');
+        $obj = SiteConfig::current_site_config();
+        if($obj->HasCaching) {
+            DB::query('
+                UPDATE "SiteConfig"
+                SET
+                    "CacheKeyLastEdited" = \'' . DBDatetime::now()->Rfc2822() . '\',
+                    "ClassNameLastEdited" = \'' . addslashes($className). '\'
+                WHERE ID = '.$obj->ID.'
+            ;');
+        }
+        if($obj->RecordCacheUpdates) {
+            $record = Injector::inst()
+                ->create(ObjectsUpdated::class, ['ClassNameLastEdited' => $className])
+                ->write();
+        }
     }
 }
---
+++ src/Model/ObjectsUpdated.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Sunnysideup\SimpleTemplateCaching\Model;
+
+use SilverStripe\ORM\DataObject;
+
+use SilverStripe\Core\Injector\Injector;
+
+/**
+ * A blog category for generalising blog posts.
+ *
+*
+ * @method Blog Blog()
+ *
+ * @property string $Title
+ * @property string $URLSegment
+ * @property int $BlogID
+ */
+class ObjectsUpdated extends DataObject
+{
+
+    /**
+     * {@inheritDoc}
+     * @var string
+     */
+    private static $table_name = 'ObjectsUpdated';
+
+    /**
+     * @var array
+     */
+    private static $db = [
+        'ClassNameLastEdited'      => 'Varchar(255)',
+    ];
+
+    private static $default_sort = [
+        'ID' => 'DESC',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $summary_fields = [
+        'Created'      => 'Updated',
+        'ClassNameLastEdited'      => 'ClassName',
+        'ClassNameTitle'      => 'Human readable name',
+    ];
+
+    /**
+     * @var array
+     */
+    private static $field_labels = [
+        'Created'      => 'Updated',
+        'ClassNameLastEdited'      => 'Code name',
+        'ClassNameTitle'      => 'Human readable name',
+    ];
+    /**
+     * @var array
+     */
+    private static $casting = [
+        'ClassNameTitle'      => 'Varchar',
+    ];
+
+    public function getClassNameTitle() : string
+    {
+        if(class_exists($this->getOwner()->ClassNameLastEdited)) {
+            return Injector::inst()->get($this->getOwner()->ClassNameLastEdited)->i18n_singular_name();
+        }
+        return 'class not found';
+    }
+}
